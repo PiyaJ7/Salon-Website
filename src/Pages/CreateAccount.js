9f1a19b65a3ffff33996397ad2700e18672f2c11
E M P J De Saram <jayodya7desaram@gmail.com>
2023-12-26 12:08:35 +0530

    Update create account page

--- src/Pages/CreateAccount.js
+++ src/Pages/CreateAccount.js
@@ -4,27 +4,37 @@ import close from './images/close.png';
 
 export default function CreateAccount() {
   return (
-    <div className="createAccount-page">
         <div className="createAccount-bg">
-            <div className="createAccount-container">
-                <button className='close-button'><img className='close-icon' src={close}/></button>
-                <h1>Create your Account</h1>
-                <form action="/action_page.php">
-                    <label for="email">Enter your email address</label><br/>
-                    <input type="text" id="email" name="email"/><br/><br/>
-                    <label for="username">Enter a username</label><br/>
-                    <input type="text" id="username" name="username"/><br/><br/>
-                    <label for="password">Enter a Password</label><br/>
-                    <input type="text" id="password" name="password" placeholder='xxxxxxxxxxx'/><br/><br/>
-                    <label for="confirm-password">Confirm Password</label><br/>
-                    <input type="text" id="confirm-password" name="confirm-password" placeholder='xxxxxxxxxxx'/><br/><br/>
-                    <input type="checkbox" id='terms-policy'/>
-                    <label for="terms-policy" id='terms-policy-label'>Agree to our terms of service and privacy policy.</label>
-                    <button className='create-account-button'>Create Account</button>
-                </form>
-                <p className='create-account-p1'>Already have an account? <strong>Login</strong></p>
+            <div className="createAccount-container">                
+                <form action="">
+                  <button className='close-button'><img className='close-icon' src={close}/></button>
+                  <h1>Create your Account</h1>
+
+                  <div className="input-box">                    
+                    <input type="text" id="email" name="email" required/>
+                    <label for="email">Enter your email address</label>
+                  </div>
+
+                  <div className="input-box">
+                    <input type="text" id="username" name="username" required/>
+                    <label for="username">Enter a username</label>                  
+                  </div>
+
+                  <div className="input-box">
+                    <input type="password" id="password" name="password" required/>
+                    <label for="password">Enter a Password</label>
+                  </div>
+
+                  <div className="input-box">
+                    <input type="password" id="confirm-password" name="confirm-password" required/>
+                    <label for="confirm-password">Confirm Password</label>
+                  </div>
+                  
+                  <label for="terms-policy" id='terms-policy-label'><input type="checkbox" id='terms-policy'/> Agree to our terms of service and privacy policy.</label>
+                  <button className='create-account-button'>Create Account</button>
+                  <p className='create-account-p1'>Already have an account? <strong>Login</strong></p>
+                </form>                
             </div>
         </div>
-    </div>
   )
 }
