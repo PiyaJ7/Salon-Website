81769544a40b5e2a0797e1cee503b431f0b61f0a
E M P J De Saram <jayodya7desaram@gmail.com>
2024-01-15 00:21:46 +0530

    Design dashboard

--- Frontend/src/App.js
+++ Frontend/src/App.js
@@ -1,26 +1,37 @@
-import './App.css';
-import CreateAccount from './Pages/CreateAccount';
-import LoginPage from './Pages/LoginPage';
-import Header from './Components/Header';
-import { BrowserRouter, Route, Routes, } from "react-router-dom";
-import PackageManagement from './Pages/PackageManagement';
-import CreatePackages from './Pages/CreatePackages';
-import ServiceManagement from './Pages/ServiceManagement';
-import Home from './Pages/Home';
-import AttendanceDetals from './Pages/AttendanceDetals';
+import "./App.css";
+import CreateAccount from "./Pages/CreateAccount";
+import LoginPage from "./Pages/LoginPage";
+import Header from "./Components/Header";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import PackageManagement from "./Pages/PackageManagement";
+import CreatePackages from "./Pages/CreatePackages";
+import ServiceManagement from "./Pages/ServiceManagement";
+import Home from "./Pages/Home";
+import AttendanceDetals from "./Pages/AttendanceDetals";
+import Dashboard from "./Pages/Dashboard";
 
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
         <Routes>
-          <Route path="/" element={<Home/>}></Route>
-          <Route path="/LoginPage" element={<LoginPage/>}></Route>
-          <Route path="/CreateAccount" element={<CreateAccount/>}></Route>
-          <Route path="/PackageManagement" element={<PackageManagement/>}></Route>
-          <Route path="/CreatePackages" element={<CreatePackages/>}></Route>
-          <Route path="/ServiceManagement" element={<ServiceManagement/>}></Route>
-          <Route path="/AttendanceDetals" element={<AttendanceDetals/>}></Route>
+          <Route path="/" element={<Home />}></Route>
+          <Route path="/LoginPage" element={<LoginPage />}></Route>
+          <Route path="/CreateAccount" element={<CreateAccount />}></Route>
+          <Route path="/Dashboard" element={<Dashboard />}></Route>
+          <Route
+            path="/PackageManagement"
+            element={<PackageManagement />}
+          ></Route>
+          <Route path="/CreatePackages" element={<CreatePackages />}></Route>
+          <Route
+            path="/ServiceManagement"
+            element={<ServiceManagement />}
+          ></Route>
+          <Route
+            path="/AttendanceDetals"
+            element={<AttendanceDetals />}
+          ></Route>
         </Routes>
       </BrowserRouter>
     </div>
--- Frontend/src/Pages/Home.js
+++ Frontend/src/Pages/Home.js
@@ -1,133 +1,89 @@
-import React from 'react'
-import './home.css';
+import React from "react";
+import "./home.css";
 import { TiThMenu } from "react-icons/ti";
-import { Link } from 'react-router-dom';
-import Header from '../Components/Header';
+import { Link } from "react-router-dom";
+import Header from "../Components/Header";
 
 export default function Home() {
   return (
     <div>
-        <Header/>
-        <div className="home-page">
+      <Header />
+      <div className="home-page">
         <div className="serviceManagement-header">
-            <div className="serviceManagement-header-left">
-                <TiThMenu size={29} className='menu-icon'/>
-                <h1>Home</h1>
-            </div>
-            <div className="serviceManagement-header-right">
-                
-            </div>
+          <div className="serviceManagement-header-left">
+            <TiThMenu size={29} className="menu-icon" />
+            <h1>Home</h1>
+          </div>
+          <div className="serviceManagement-header-right"></div>
         </div>
         <div className="serviceManagement-body">
-            <div className="block-body">
+          <div className="block-body">
             <Link to="/LoginPage">
-                <div className="blocks">
-                    Login Page
-                </div>
+              <div className="blocks">Login Page</div>
             </Link>
             <Link to="/CreateAccount">
-                <div className="blocks">
-                    Create Account
-                </div>
+              <div className="blocks">Create Account</div>
             </Link>
             <Link to="/Dashboard">
-                <div className="blocks">
-                    Dashboard
-                </div>
+              <div className="blocks">Dashboard</div>
             </Link>
             <Link to="/PackageManagement">
-                <div className="blocks">
-                    Package Management
-                </div>
+              <div className="blocks">Package Management</div>
             </Link>
             <Link to="/CreatePackages">
-                <div className="blocks">
-                    Create new package
-                </div>
+              <div className="blocks">Create new package</div>
             </Link>
             <Link to="/ServiceManagement">
-                <div className="blocks">
-                    Service Management
-                </div>
+              <div className="blocks">Service Management</div>
             </Link>
             <Link to="/AttendanceDetals">
-                <div className="blocks">
-                    Attendance details
-                </div>
+              <div className="blocks">Attendance details</div>
             </Link>
             <Link to="/LoginPage">
-                <div className="blocks">
-                    Schedule Management
-                </div>
+              <div className="blocks">Schedule Management</div>
             </Link>
             <Link to="/LoginPage">
-                <div className="blocks">
-                Create Schedule
-                </div>
+              <div className="blocks">Create Schedule</div>
             </Link>
             <Link to="/LoginPage">
-                <div className="blocks">
-                Inventory Mangement
-                </div>
+              <div className="blocks">Inventory Mangement</div>
             </Link>
             <Link to="/LoginPage">
-                <div className="blocks">
-                    Finance Management
-                </div>
+              <div className="blocks">Finance Management</div>
             </Link>
             <Link to="/LoginPage">
-                <div className="blocks">
-                    Appointment Management
-                </div>
+              <div className="blocks">Appointment Management</div>
             </Link>
             <Link to="/LoginPage">
-                <div className="blocks">
-                    Employee Management
-                </div>
+              <div className="blocks">Employee Management</div>
             </Link>
             <Link to="/LoginPage">
-                <div className="blocks">
-                    Add employee
-                </div>
+              <div className="blocks">Add employee</div>
             </Link>
             <Link to="/LoginPage">
-                <div className="blocks">
-                    Salary details
-                </div>
+              <div className="blocks">Salary details</div>
             </Link>
             <Link to="/LoginPage">
-                <div className="blocks">
-                    Add salary
-                </div>
+              <div className="blocks">Add salary</div>
             </Link>
             <Link to="/LoginPage">
-                <div className="blocks">
-                    Supplier Management
-                </div>
+              <div className="blocks">Supplier Management</div>
             </Link>
             <Link to="/LoginPage">
-                <div className="blocks">
-                    Supplier details
-                </div>
+              <div className="blocks">Supplier details</div>
             </Link>
             <Link to="/LoginPage">
-                <div className="blocks">
-                    Add supplier
-                </div>
+              <div className="blocks">Add supplier</div>
             </Link>
             <Link to="/LoginPage">
-                <div className="blocks">
-                    Order details
-                </div>
+              <div className="blocks">Order details</div>
             </Link>
             <Link to="/LoginPage">
-                <div className="blocks">
-                    Add order
-                </div>
+              <div className="blocks">Add order</div>
             </Link>
-            </div>            
+          </div>
         </div>
+      </div>
     </div>
-    </div>
-  )
+  );
 }
