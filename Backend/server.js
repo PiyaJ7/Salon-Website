d9b59117b5e7b0790da10659e506f0db2545485e
DevinHansa <devinhansa@gmail.com>
2024-01-16 22:48:50 +0530

    portFinder

--- Backend/server.js
+++ Backend/server.js
@@ -64,14 +64,21 @@ app.get("/", (req, res) => {
 
 // Find an available port dynamically
 // Install on backend npm install portfinder
-portfinder
-  .getPortPromise({ startPort: 3002 })
-  .then((port) => {
-    // Start the server on the dynamically found port
-    app.listen(port, () => {
-      console.log(`Server is listening on port ${port}`);
-    });
-  })
-  .catch((err) => console.error("Error finding available port:", err));
+// portfinder
+//   .getPortPromise({ startPort: 3002 })
+//   .then((port) => {
+//     // Start the server on the dynamically found port
+//     app.listen(port, () => {
+//       console.log(`Server is listening on port ${port}`);
+//     });
+//   })
+//   .catch((err) => console.error("Error finding available port:", err));
+
+// Start the server on port 8000
+const PORT = 8000;
+app.listen(PORT, () => {
+  console.log(`Server is listening on port ${PORT}`);
+});
+
 
 //serviceRoutes.json
