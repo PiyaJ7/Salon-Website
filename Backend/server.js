8458105ca277647075f5f4818c0b9a3e42c10d94
DevinHansa <devinhansa@gmail.com>
2024-01-16 23:54:15 +0530

    order

--- Backend/server.js
+++ Backend/server.js
@@ -27,7 +27,7 @@ const empRoutes = require('./routes/employeeRoutes');
 const inventoryRoutes = require('./routes/inventoryRoutes');
 // const financeRoutes = require('./routes/financeRoutes');
 const inveMsgRoutes = require('./routes/inveMsgRoutes');
-// const orderRoutes = require('./routes/orderRoutes');
+const orderRoutes = require('./routes/orderRoutes');
 // const salRoutes = require('./routes/salaryRoutes');
 // const scheduleRoutes = require('./routes/scheduleRoutes');
 // const supMsgRoutes = require('./routes/supMsgRoutes');
@@ -43,7 +43,7 @@ app.use("/api/employees", empRoutes);
 app.use("/api/Product", inventoryRoutes);
 //app.use("/api/Fin", financeRoutes);
 app.use("/api/IMsg", inveMsgRoutes);
-// app.use("/api/Ord", orderRoutes)
+app.use("/api/Ord", orderRoutes)
 // app.use("/api/sal", salRoutes);
 // app.use("/api/schedu", scheduleRoutes);
 // app.use("/api/Msg", supMsgRoutes);
