46ea46aaa791f25e36acb82db61a52cc920e7e80
DevinHansa <devinhansa@gmail.com>
2024-01-16 22:39:28 +0530

    server

--- Backend/server.js
+++ Backend/server.js
@@ -21,33 +21,33 @@ const packageRoutes = require("./routes/packageRoutes");
 
 //const packageRoutes = require('./routes/packageRoutes');
 
-//const serviceRoutes = require('./routes/serviceRoutes');
-//const attendanceRoutes = require('./routes/attendanceRoutes');
+const serviceRoutes = require('./routes/serviceRoutes');
+const attendanceRoutes = require('./routes/attendanceRoutes');
 const empRoutes = require("./routes/employeeRoutes");
 const inventoryRoutes = require('./routes/inventoryRoutes');
-//const financeRoutes = require('./routes/financeRoutes');
+const financeRoutes = require('./routes/financeRoutes');
 const inveMsgRoutes = require('./routes/inveMsgRoutes');
 const orderRoutes = require('./routes/orderRoutes');
-//const salRoutes = require('./routes/salaryRoutes');
-//const scheduleRoutes = require('./routes/scheduleRoutes');
-//const supMsgRoutes = require('./routes/supMsgRoutes');
-//const supplierRoutes = require('./routes/supplierRoutes')
+const salRoutes = require('./routes/salaryRoutes');
+const scheduleRoutes = require('./routes/scheduleRoutes');
+const supMsgRoutes = require('./routes/supMsgRoutes');
+const supplierRoutes = require('./routes/supplierRoutes')
 
 // Routes
 app.use("/api/users", userRoutes);
 app.use("/api/Book", appointmentRoutes);
 app.use("/api/packages", packageRoutes);
-//app.use("/api/services", serviceRoutes);
-//app.use("/api/attendances", attendanceRoutes);
+app.use("/api/services", serviceRoutes);
+app.use("/api/attendances", attendanceRoutes);
 app.use("/api/employees", empRoutes);
 app.use("/api/Product", inventoryRoutes);
-//app.use("/api/Fin", financeRoutes);
+app.use("/api/Fin", financeRoutes);
 app.use("/api/IMsg", inveMsgRoutes);
 app.use("/api/Ord", orderRoutes)
-//app.use("/api/sal", salRoutes);
-//app.use("/api/schedu", scheduleRoutes);
-//app.use("/api/Msg", supMsgRoutes);
-//app.use("/api/Sup", supplierRoutes);
+app.use("/api/sal", salRoutes);
+app.use("/api/schedu", scheduleRoutes);
+app.use("/api/Msg", supMsgRoutes);
+app.use("/api/Sup", supplierRoutes);
 
 // Connect to the MongoDB database
 mongoose
