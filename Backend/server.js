d8cd2296e73fddedfa5661e581caec44f1fdaffb
DevinHansa <devinhansa@gmail.com>
2024-01-16 23:34:57 +0530

    change

--- Backend/server.js
+++ Backend/server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 const mongoose = require('mongoose');
-const bodyParser = require('body-parser');
+// const bodyParser = require('body-parser');
 const portfinder = require('portfinder');
 
 // Create the Express app
