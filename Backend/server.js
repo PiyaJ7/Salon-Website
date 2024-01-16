494b9470850d80922f1d6d78b457177b8915a214
DevinHansa <devinhansa@gmail.com>
2024-01-16 23:10:14 +0530

    com

--- Backend/server.js
+++ Backend/server.js
@@ -25,12 +25,12 @@ const serviceRoutes = require('./routes/serviceRoutes');
 const attendanceRoutes = require('./routes/attendanceRoutes');
 const empRoutes = require("./routes/employeeRoutes");
 const inventoryRoutes = require('./routes/inventoryRoutes');
-const financeRoutes = require('./routes/financeRoutes');
-const inveMsgRoutes = require('./routes/inveMsgRoutes');
-const orderRoutes = require('./routes/orderRoutes');
-const salRoutes = require('./routes/salaryRoutes');
-const scheduleRoutes = require('./routes/scheduleRoutes');
-const supMsgRoutes = require('./routes/supMsgRoutes');
+// const financeRoutes = require('./routes/financeRoutes');
+// const inveMsgRoutes = require('./routes/inveMsgRoutes');
+// const orderRoutes = require('./routes/orderRoutes');
+// const salRoutes = require('./routes/salaryRoutes');
+// const scheduleRoutes = require('./routes/scheduleRoutes');
+// const supMsgRoutes = require('./routes/supMsgRoutes');
 const supplierRoutes = require('./routes/supplierRoutes')
 
 // Routes
@@ -41,12 +41,12 @@ app.use("/api/services", serviceRoutes);
 app.use("/api/attendances", attendanceRoutes);
 app.use("/api/employees", empRoutes);
 app.use("/api/Product", inventoryRoutes);
-app.use("/api/Fin", financeRoutes);
-app.use("/api/IMsg", inveMsgRoutes);
-app.use("/api/Ord", orderRoutes)
-app.use("/api/sal", salRoutes);
-app.use("/api/schedu", scheduleRoutes);
-app.use("/api/Msg", supMsgRoutes);
+// app.use("/api/Fin", financeRoutes);
+// app.use("/api/IMsg", inveMsgRoutes);
+// app.use("/api/Ord", orderRoutes)
+// app.use("/api/sal", salRoutes);
+// app.use("/api/schedu", scheduleRoutes);
+// app.use("/api/Msg", supMsgRoutes);
 app.use("/api/Sup", supplierRoutes);
 
 // Connect to the MongoDB database
