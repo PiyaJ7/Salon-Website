653ea300543a01d0d6ca5596db48feba647ffd64
DevinHansa <devinhansa@gmail.com>
2024-01-20 11:56:21 +0530

    create

--- Backend/routes/customPackRoutes.js
+++ Backend/routes/customPackRoutes.js
@@ -11,6 +11,24 @@ const Custom = require('../models/customPackModel');
 //         .catch((err) => console.log(err));
 // });
 
+router.post("/create", async (req, res) => {
+    try {
+        const createdCustom = await Custom.create({
+            services: req.body.services,
+            price: req.body.price
+        });
+
+        console.log(createdCustom);
+        res.json({ message: "Custom created successfully", custom: createdCustom });
+    } catch (err) {
+        console.error(err);
+        res.status(500).json({ error: "Error creating Custom" });
+    }
+});
+
+
+
+
 
 
 module.exports = router;
