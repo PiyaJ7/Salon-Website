f1d2fb5ca3ec0fb3426e310b5e7c676fdf00293c
DevinHansa <devinhansa@gmail.com>
2024-01-20 11:51:31 +0530

    update

--- Backend/routes/appointmentRoutes.js
+++ Backend/routes/appointmentRoutes.js
@@ -140,21 +140,45 @@ router.delete("/delete/:id", async (req, res) => {
 
 
 // Create API route for Update method in CRUD Operations
-router.put("/update/:id", (req, res) => {
-    Appointment.findByIdAndUpdate(
-        { _id: req.params.id },
-        {
-            name: req.body.name,
-            contact: req.body.contact,
-            email: req.body.email,
-            date: req.body.date,
-            time: req.body.time,
-            service: req.body.service,
-        }
-    )
-        .then((doc) => console.log(doc))
-        .catch((err) => console.log(err));
+// router.put("/update/:id", (req, res) => {
+//     Appointment.findByIdAndUpdate(
+//         { _id: req.params.id },
+//         {
+//             name: req.body.name,
+//             contact: req.body.contact,
+//             email: req.body.email,
+//             date: req.body.date,
+//             time: req.body.time,
+//             service: req.body.service,
+//         }
+//     )
+//         .then((doc) => console.log(doc))
+//         .catch((err) => console.log(err));
 
+// });
+
+router.put("/update/:id", async (req, res) => {
+    try {
+        const updatedAppointment = await Appointment.findByIdAndUpdate(
+            { _id: req.params.id },
+            {
+                name: req.body.name,
+                contact: req.body.contact,
+                email: req.body.email,
+                date: req.body.date,
+                time: req.body.time,
+                service: req.body.service,
+            },
+            { new: true } // To return the updated document
+        );
+
+        console.log(updatedAppointment);
+        res.send("Appointment Updated Successfully");
+    } catch (err) {
+        console.log(err);
+        res.status(500).send("Error Updating Appointment");
+    }
 });
+
 
 module.exports = router;
