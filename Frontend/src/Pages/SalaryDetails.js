84c3cc275a4259b95c22d58e2de7c26d9a5a209c
E M P J De Saram <jayodya7desaram@gmail.com>
2024-01-29 20:28:19 +0530

    Salary sheet download

--- Frontend/src/Pages/SalaryDetails.js
+++ Frontend/src/Pages/SalaryDetails.js
@@ -1,10 +1,12 @@
 import React, { useEffect, useState } from "react";
 import "./salaryDetails.css";
-import { TiThMenu } from "react-icons/ti";
 import Header from "../Components/Header";
 import { useNavigate } from "react-router-dom";
 import Sidebar from "../Components/Sidebar";
 import axios from "axios";
+import logo from "./images/logo.png";
+import jsPDF from "jspdf";
+import autoTable from "jspdf-autotable";
 
 export default function SalaryDetails() {
   const navigate = useNavigate();
@@ -21,6 +23,63 @@ export default function SalaryDetails() {
     navigate("/AddSalary");
   };
 
+  const downloadPDF = async () => {
+    try {
+      const response = await axios.get("http://localhost:8000/api/sal/sals");
+      const SalaryData = response.data;
+
+      const doc = new jsPDF();
+
+      doc.addImage(logo, "PNG", 10, 5, 40, 40);
+
+      const headerX = doc.internal.pageSize.width - 20;
+
+      doc.setFontSize(14);
+      doc.text("Salon JAYDE", headerX, 20, { align: "right" });
+
+      doc.setFontSize(12);
+      doc.text("43/8, Flower Road, Colombo 07", headerX, 27, {
+        align: "right",
+      });
+      doc.setFontSize(10);
+      doc.text("077-3526412/071-5263491", headerX, 32, {
+        align: "right",
+      });
+
+      doc.setLineWidth(0.5);
+      doc.line(8, 42, 200, 42);
+
+      doc.setFont("bold");
+      doc.setFontSize(20);
+      doc.text("Salary Sheet", 80, 60);
+      doc.setFont("normal");
+
+      doc.setDrawColor(0);
+
+      const columns = [
+        { header: "Employee ID", dataKey: "empID" },
+        { header: "Month", dataKey: "month" },
+        { header: "Working Dates", dataKey: "wdays" },
+        { header: "Pay Rate(LKR)", dataKey: "rate" },
+        { header: "Net Salary(LKR)", dataKey: "netSalary" },
+      ];
+
+      const rows = SalaryData.map((employee) => ({
+        empID: employee.id,
+        month: employee.month,
+        wdays: employee.workingDays,
+        rate: employee.payRate,
+        netSalary: employee.workingDays * employee.payRate,
+      }));
+
+      autoTable(doc, { columns, body: rows, startY: 70 });
+
+      doc.save("Salary sheet.pdf");
+    } catch (error) {
+      console.error("Error fetching or generating PDF:", error);
+    }
+  };
+
   return (
     <div>
       <Header />
@@ -34,7 +93,7 @@ export default function SalaryDetails() {
             <button onClick={addSalaryClick} className="add-salary-button">
               Add salary
             </button>
-            <button className="download-salary-button">
+            <button onClick={downloadPDF} className="download-salary-button">
               Download salary sheet
             </button>
           </div>
